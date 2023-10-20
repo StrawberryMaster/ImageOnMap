09a56e45bc2cbfda197d8efd733af0afce4c544d
TehBrian <tehbrian@proton.me>
2023-10-19 23:11:57 -0400

    fix statistics in maps GUI lacking item meta

--- src/main/java/fr/moribus/imageonmap/gui/MapListGui.java
+++ src/main/java/fr/moribus/imageonmap/gui/MapListGui.java
@@ -1,39 +1,3 @@
-/*
- * Copyright or © or Copr. Moribus (2013)
- * Copyright or © or Copr. ProkopyL <prokopylmc@gmail.com> (2015)
- * Copyright or © or Copr. Amaury Carrade <amaury@carrade.eu> (2016 – 2021)
- * Copyright or © or Copr. Vlammar <valentin.jabre@gmail.com> (2019 – 2021)
- *
- * This software is a computer program whose purpose is to allow insertion of
- * custom images in a Minecraft world.
- *
- * This software is governed by the CeCILL license under French law and
- * abiding by the rules of distribution of free software.  You can  use,
- * modify and/ or redistribute the software under the terms of the CeCILL
- * license as circulated by CEA, CNRS and INRIA at the following URL
- * "http://www.cecill.info".
- *
- * As a counterpart to the access to the source code and  rights to copy,
- * modify and redistribute granted by the license, users are provided only
- * with a limited warranty  and the software's author,  the holder of the
- * economic rights,  and the successive licensors  have only  limited
- * liability.
- *
- * In this respect, the user's attention is drawn to the risks associated
- * with loading,  using,  modifying and/or developing or reproducing the
- * software by the user in light of its specific status of free software,
- * that may mean  that it is complicated to manipulate,  and  that  also
- * therefore means  that it is reserved for developers  and  experienced
- * professionals having in-depth computer knowledge. Users are therefore
- * encouraged to load and test the software's suitability as regards their
- * requirements in conditions enabling the security of their systems and/or
- * data to be ensured and,  more generally, to use and operate it in the
- * same conditions as regards security.
- *
- * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL license and that you accept its terms.
- */
-
 package fr.moribus.imageonmap.gui;
 
 import dev.tehbrian.imageonmap.Permission;
@@ -45,10 +9,7 @@ import fr.moribus.imageonmap.map.PosterMap;
 import fr.moribus.imageonmap.map.SingleMap;
 import fr.moribus.imageonmap.ui.MapItemManager;
 import fr.moribus.imageonmap.ui.SplatterMapManager;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
@@ -57,17 +18,19 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.MapMeta;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MapListGui extends ExplorerGui<ImageMap> {
-    private final OfflinePlayer offplayer;
+    private final OfflinePlayer player;
     private final String name;
 
     public MapListGui(OfflinePlayer p, String name) {
-        this.offplayer = p;
+        this.player = p;
         this.name = name;
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     protected ItemStack getViewItem(ImageMap map) {
         String mapDescription;
@@ -89,7 +52,7 @@ public class MapListGui extends ExplorerGui<ImageMap> {
         ItemStack mapItem = new ItemStack(Material.FILLED_MAP);
         MapMeta meta = (MapMeta) mapItem.getItemMeta();
 
-        meta.setDisplayName(I.tl(getPlayerLocale(), "{green}{bold}{0}", map.getName()));
+        meta.displayName(Component.text(I.tl(getPlayerLocale(), "{green}{bold}{0}", map.getName())));
         List<String> lore = new ArrayList<>(Arrays.asList(
                 mapDescription,
                 "",
@@ -100,7 +63,7 @@ public class MapListGui extends ExplorerGui<ImageMap> {
             lore.add(I.tl(getPlayerLocale(), "{gray}» {white}Left-click{gray} to get this map"));
         }
         lore.add(I.tl(getPlayerLocale(), "{gray}» {white}Right-click{gray} for details and options"));
-        meta.setLore(lore);
+        meta.lore(lore.stream().map(Component::text).toList());
 
         meta.setColor(Color.GREEN);
         mapItem.setItemMeta(meta);
@@ -108,13 +71,12 @@ public class MapListGui extends ExplorerGui<ImageMap> {
         return mapItem;
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     protected ItemStack getEmptyViewItem() {
         ItemStack item = new ItemStack(Material.MAP);
         ItemMeta meta = item.getItemMeta();
-        if (offplayer.getUniqueId().equals(getPlayer().getUniqueId())) {
-            meta.setDisplayName(I.tl(getPlayerLocale(), "{red}You don't have any map."));
+        if (player.getUniqueId().equals(getPlayer().getUniqueId())) {
+            meta.displayName(Component.text(I.tl(getPlayerLocale(), "{red}You don't have any map.")));
 
             List<String> lore = new ArrayList<>();
             if (Permission.NEW.grantedTo(getPlayer())) {
@@ -125,10 +87,10 @@ public class MapListGui extends ExplorerGui<ImageMap> {
                         "{gray}Unfortunately, you are not allowed to create one.")));
             }
             if (!lore.isEmpty()) {
-                meta.setLore(lore);
+                meta.lore(lore.stream().map(Component::text).toList());
             }
         } else {
-            meta.setDisplayName(I.tl(getPlayerLocale(), "{red}{0} doesn't have any map.", name));
+            meta.displayName(Component.text(I.tl(getPlayerLocale(), "{red}{0} doesn't have any map.", name)));
         }
 
         item.setItemMeta(meta);
@@ -161,14 +123,13 @@ public class MapListGui extends ExplorerGui<ImageMap> {
         return null;
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     protected void onUpdate() {
-        ImageMap[] maps = MapManager.getMaps(offplayer.getUniqueId());
+        ImageMap[] maps = MapManager.getMaps(player.getUniqueId());
         setData(maps);
         /// The maps list GUI title
         //Equal if the person who send the command is the owner of the mapList
-        if (offplayer.getUniqueId().equals(getPlayer().getUniqueId())) {
+        if (player.getUniqueId().equals(getPlayer().getUniqueId())) {
             setTitle(I.tl(getPlayerLocale(), "{black}Your maps {reset}({0})", maps.length));
         } else {
             setTitle(I.tl(getPlayerLocale(), "{black}{1}'s maps {reset}({0})", maps.length, name));
@@ -178,7 +139,7 @@ public class MapListGui extends ExplorerGui<ImageMap> {
 
 
         /* ** Statistics ** */
-        int mapPartCount = MapManager.getMapPartCount(offplayer.getUniqueId());
+        int mapPartCount = MapManager.getMapPartCount(player.getUniqueId());
 
         int mapGlobalLimit = PluginConfiguration.MAP_GLOBAL_LIMIT.get();
         int mapPersonalLimit = PluginConfiguration.MAP_PLAYER_LIMIT.get();
@@ -197,13 +158,13 @@ public class MapListGui extends ExplorerGui<ImageMap> {
             mapPartLeft = Math.min(mapPartGloballyLeft, mapPartPersonallyLeft);
         }
 
-        int imagesCount = MapManager.getMapList(offplayer.getUniqueId()).size();
+        int imagesCount = MapManager.getMapList(player.getUniqueId()).size();
         double percentageUsed =
                 mapPartLeft < 0 ? 0 : ((double) mapPartCount) / ((double) (mapPartCount + mapPartLeft)) * 100;
 
         ItemStack statistics = new ItemStack(Material.ENCHANTED_BOOK);
         ItemMeta meta = statistics.getItemMeta();
-        meta.setDisplayName(I.t(getPlayerLocale(), "{blue}Usage statistics"));
+        meta.displayName(Component.text(I.t(getPlayerLocale(), "{blue}Usage statistics")));
         List<String> lore = new ArrayList<>();
         lore.add("");
         lore.add(I.tn(getPlayerLocale(),
@@ -224,8 +185,9 @@ public class MapListGui extends ExplorerGui<ImageMap> {
             lore.add(I.tn(getPlayerLocale(), "{white}{0}{gray} map left", "{white}{0}{gray} maps left",
                     mapPartLeft));
         }
-        meta.setLore(lore);
+        meta.lore(lore.stream().map(Component::text).toList());
         meta.addItemFlags(ItemFlag.values());
+        statistics.setItemMeta(meta);
 
         action("", getSize() - 5, statistics);
     }
